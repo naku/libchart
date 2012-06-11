e464fa7f83b93ab220403a1cb4ff1f63b4fbe41b
jm.tremeaux <jm.tremeaux@localhost>
2012-06-11 10:42:43 +0200

    Fix pie chart color when null value is present

--- libchart/classes/view/chart/PieChart.php
+++ libchart/classes/view/chart/PieChart.php
@@ -1,6 +1,6 @@
 <?php
     /* Libchart - PHP chart library
-     * Copyright (C) 2005-2011 Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
+     * Copyright (C) 2005-2011 Jean-Marc Trï¿½meaux (jm.tremeaux at gmail.com)
      * 
      * This program is free software: you can redistribute it and/or modify
      * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
     /**
      * Pie chart.
      *
-     * @author Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
+     * @author Jean-Marc Trï¿½meaux (jm.tremeaux at gmail.com)
      */
     class PieChart extends Chart {
         protected $pieCenterX;
@@ -163,6 +163,7 @@
 
                 // If value is null, don't draw this arc
                 if ($percent <= 0) {
+                    $i++;
                     continue;
                 }
                 
