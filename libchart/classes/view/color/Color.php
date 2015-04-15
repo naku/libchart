3fa48c40dafdb08fe0eb1eb441339fac8c99169b
Jean-Marc Tremeaux <jm.tremeaux@gmail.com>
2015-04-15 10:43:49 +0200

    Fix encoding

--- libchart/classes/view/color/Color.php
+++ libchart/classes/view/color/Color.php
@@ -1,6 +1,6 @@
 <?php
     /* Libchart - PHP chart library
-     * Copyright (C) 2005-2011 Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
+     * Copyright (C) 2005-2011 Jean-Marc Tremeaux (jm.tremeaux at gmail.com)
      * 
      * This program is free software: you can redistribute it and/or modify
      * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@
     /**
      * Color.
      *
-     * @author Jean-Marc Trémeaux (jm.tremeaux at gmail.com)
+     * @author Jean-Marc Tremeaux (jm.tremeaux at gmail.com)
      */
     class Color {
         private $red;
