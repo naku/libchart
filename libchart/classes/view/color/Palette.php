4a774060e89721e9c16b9f6795b4db492526e793
kastaldi <kastaldi@users.noreply.github.com>
2019-01-08 20:46:48 +0100

    Class modified for PHP 7  compatibility

--- libchart/classes/view/color/Palette.php
+++ libchart/classes/view/color/Palette.php
@@ -37,7 +37,7 @@
         /**
          * Palette constructor.
          */
-        public function Palette() {
+        public function __construct() {
             $this->red = new Color(255, 0, 0);
         
             // Set the colors for the horizontal and vertical axis
